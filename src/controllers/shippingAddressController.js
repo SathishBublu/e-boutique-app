3a336ac0b29e7dfe0673887cf329efc6059f313b
SathishBublu <sathishvm37@gmail.com>
2021-04-20 17:39:27 +0530

    refactor: shippingAddress with checkOwnerShip helper

--- src/controllers/shippingAddressController.js
+++ src/controllers/shippingAddressController.js
@@ -6,11 +6,7 @@ const User = require('../models/userModel');
 const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 const pick = require('../utils/pick');
-
-const checkOwnerShip = (requestedUserId, loggedUser, nextFunction) => {
-  if (requestedUserId !== loggedUser.id && loggedUser.role !== 'admin')
-    return nextFunction(new AppError('You are not allowed to perfom this action', httpStatus.FORBIDDEN));
-};
+const helpers = require('../utils/helpers');
 
 exports.getAllShippingAddress = catchAsync(async (req, res, next) => {
   const shippingAddress = await ShippingAddress.find();
@@ -27,76 +23,92 @@ exports.getAllShippingAddress = catchAsync(async (req, res, next) => {
 exports.getShippingAddress = catchAsync(async (req, res, next) => {
   const { userId } = req.params;
 
-  checkOwnerShip(userId, req.user, next);
+  const isOwner = helpers.checkOwnerShip(userId, req.user);
 
-  const shippingAddress = await ShippingAddress.findOne({ userId });
+  if (isOwner) {
+    const shippingAddress = await ShippingAddress.findOne({ userId });
 
-  if (!shippingAddress) return next(new AppError('There is no shipping address found.', httpStatus.NOT_FOUND));
+    if (!shippingAddress) return next(new AppError('There is no shipping address found.', httpStatus.NOT_FOUND));
 
-  res.status(httpStatus.OK).json({
-    status: 'success',
-    data: {
-      shippingAddress,
-    },
-  });
+    return res.status(httpStatus.OK).json({
+      status: 'success',
+      data: {
+        shippingAddress,
+      },
+    });
+  }
+  next(new AppError('You are not allowed to see other shipping address.', httpStatus.FORBIDDEN));
 });
 
 exports.createShippingAddress = catchAsync(async (req, res, next) => {
   const filteredData = pick(req.body, ShippingAddress.schema.requiredPaths());
 
-  if (filteredData.userId) checkOwnerShip(filteredData.userId, req.user, next);
-  if (!filteredData.userId) filteredData.userId = req.user.id;
+  let isOwner;
+  if (filteredData.userId) isOwner = helpers.checkOwnerShip(filteredData.userId, req.user, next);
 
-  const shippingAddressExists = await ShippingAddress.findOne({ userId: filteredData.userId });
+  if (isOwner) {
+    if (!filteredData.userId) filteredData.userId = req.user.id;
 
-  if (shippingAddressExists) return next(new AppError('Shipping Address already Exists', httpStatus.BAD_REQUEST));
+    const shippingAddressExists = await ShippingAddress.findOne({ userId: filteredData.userId });
 
-  const newShippingAddress = await ShippingAddress.create(filteredData);
-  await User.findByIdAndUpdate(filteredData.userId, { address: newShippingAddress.id });
+    if (shippingAddressExists) return next(new AppError('Shipping Address already Exists', httpStatus.BAD_REQUEST));
 
-  res.status(httpStatus.CREATED).json({
-    status: 'success',
-    data: {
-      shippingAddress: newShippingAddress,
-    },
-  });
+    const newShippingAddress = await ShippingAddress.create(filteredData);
+    await User.findByIdAndUpdate(filteredData.userId, { address: newShippingAddress.id });
+
+    return res.status(httpStatus.CREATED).json({
+      status: 'success',
+      data: {
+        shippingAddress: newShippingAddress,
+      },
+    });
+  }
+
+  next(new AppError('You are not allowed to create shipping address with other ID.'), httpStatus.FORBIDDEN);
 });
 
 exports.updateShippingAddress = catchAsync(async (req, res, next) => {
   const { userId } = req.params;
 
-  checkOwnerShip(userId, req.user, next);
+  const isOwner = helpers.checkOwnerShip(userId, req.user);
 
-  const shippingAddressExists = await ShippingAddress.findOne({ userId });
+  if (isOwner) {
+    const shippingAddressExists = await ShippingAddress.findOne({ userId });
 
-  if (!shippingAddressExists) return next(new AppError('There is no shipping address found.', httpStatus.NOT_FOUND));
+    if (!shippingAddressExists) return next(new AppError('There is no shipping address found.', httpStatus.NOT_FOUND));
 
-  const filteredData = pick(req.body, ShippingAddress.schema.requiredPaths());
+    const filteredData = pick(req.body, ShippingAddress.schema.requiredPaths());
 
-  const updatedShippingAddress = await ShippingAddress.findOneAndUpdate({ userId }, filteredData, {
-    new: true,
-    runValidators: true,
-  });
+    const updatedShippingAddress = await ShippingAddress.findOneAndUpdate({ userId }, filteredData, {
+      new: true,
+      runValidators: true,
+    });
 
-  res.status(httpStatus.ACCEPTED).send({
-    status: 'success',
-    data: {
-      shippingAddress: updatedShippingAddress,
-    },
-  });
+    return res.status(httpStatus.ACCEPTED).send({
+      status: 'success',
+      data: {
+        shippingAddress: updatedShippingAddress,
+      },
+    });
+  }
+  next(new AppError("You are not allowed to update other's shipping address.", httpStatus.FORBIDDEN));
 });
 
 exports.deleteShippingAddress = catchAsync(async (req, res, next) => {
   const { userId } = req.params;
 
-  checkOwnerShip(userId, req.user, next);
+  const isOwner = helpers.checkOwnerShip(userId, req.user);
 
-  const deletedShippingAddress = await ShippingAddress.findOneAndDelete({ userId });
+  if (isOwner) {
+    const deletedShippingAddress = await ShippingAddress.findOneAndDelete({ userId });
 
-  if (!deletedShippingAddress) return next(new AppError('There is no shipping Address found', httpStatus.NOT_FOUND));
+    if (!deletedShippingAddress) return next(new AppError('There is no shipping Address found', httpStatus.NOT_FOUND));
 
-  res.status(httpStatus.ACCEPTED).json({
-    status: 'success',
-    message: 'Shipping address deleted successfully',
-  });
+    return res.status(httpStatus.ACCEPTED).json({
+      status: 'success',
+      message: 'Shipping address deleted successfully',
+    });
+  }
+
+  next(new AppError("You are not allowed to delete other's shipping address", httpStatus.FORBIDDEN));
 });
