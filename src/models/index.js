1aad94d1c55e030d3816e5cea491141dc7402427
SathishBublu <sathishvm37@gmail.com>
2021-04-21 23:22:57 +0530

    Add wishList in model/index file

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.Product = require('./productModel');
 module.exports.User = require('./userModel');
 module.exports.Review = require('./reviewModel');
 module.exports.ShippingAddress = require('./shippingAddressModel');
+module.exports.WishList = require('./wishListModel');
