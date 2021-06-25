4a9772c8c1875be2478178e8986a138a661c6793
SathishBublu <sathishvm37@gmail.com>
2021-06-25 21:40:56 +0530

    feat: isNumeric helper added

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,17 @@
 /**
- * Check ownership before proceed to next action.
+ * Checks ownership before proceed to next action.
  * @param {string} requestedUserId - Requested user id
  * @param {object} loggedUser - Logged in User details
- * @returns {boolean}
+ * @returns boolean
  */
 exports.checkOwnerShip = (requestedUserId, loggedUser) => {
   if (requestedUserId !== loggedUser.id && loggedUser.role !== 'admin') return false;
   return true;
 };
+
+/**
+ * Checks the input is a number or a string.
+ * @param {string} value - Either a string nor a number
+ * @returns boolean
+ */
+exports.isNumeric = (value) => !Number.isNaN(value) && !Number.isNaN(parseFloat(value));
